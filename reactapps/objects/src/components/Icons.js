ad018d371dcaaa8fe2f92a8226f97912bc4c9d16
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2019-03-30 21:04:22 -0600

    update march 30th

---
+++ reactapps/objects/src/components/Icons.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Icons extends Component {
+  render() {
+    return(
+      <div>
+        <i class="fab fa-wolf-pack-battalion"></i>
+        <i class="fab fa-phoenix-squadron"></i>
+        <i class="fab fa-studiovinari"></i>
+        <i class="fab fa-gitkraken"></i>
+      </div>
+    );
+  }
+}
+
+export default Icons;

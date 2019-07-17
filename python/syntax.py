2f9890beaa46216d75f32d12ff9dc4cff00abe38
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2019-07-17 15:30:36 -0600

    $practice consolidation

---
+++ python/syntax.py
@@ -0,0 +1,41 @@
+## Attributes/Variables
+# Intergers - Whole Numbers
+int1 = 2
+int2 = -36
+int3 = 678
+
+print(int1, int2, int3)
+
+# Floating Point - Numbers with a decimal place
+float1 = 0.76
+float2 = 7.65
+float3 = 28.0
+
+print(float1, float2, float3)
+
+# Strings - Ordered sequence of characters,
+str1 = 'This is a string' # Indicated by single quotes ('')
+str2 = "8736457" # or double quotes ("")
+str3 = "'This is also a string'" # <-- In this sequence, the inner single quotes ('') are part of the string
+
+print(str1, str2, str3)
+
+# Booleans - Logical value that is either true or false (binary)
+bool1 = (2 == 2) #should return True
+bool2 = ('8' == 8) #should return False - in JavaScript, this would return True
+bool3 = (3 > 9) #should return False
+
+print(bool1, bool2, bool3)
+
+# Lists - Ordered sequence of items, can be mixed types
+list1 = [1,2,3]
+list2 = [45, 'apple', True, 5.72]
+
+print(list1, list2)
+
+# Lists can also contain other Lists
+megalist = [list1, list2]
+
+print(megalist)
+
+# Dictionaries - 
